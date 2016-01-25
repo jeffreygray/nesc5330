672ba0d63a5bddc94a9203441d8269d0cbbe1970
Octave Online User <webmaster@octave-online.net>
2016-01-25 02:19:38 +0000

    Scripted user file commit

--- stats.m
+++ stats.m
@@ -1,1 +1,17 @@
-disp("Hello World");
+##  jeff gray
+##  01.24.2016
+##  nesc5330
+##  lab1
+##  file: 'hw1-2-1.m' 
+##  desc: 'calculates the mean, var, and sd for a vector'
+
+function output = stats (dataset)
+    sampleMean = mean(dataset);
+    sampleVar = var(dataset);
+    sampleStd = std(dataset);
+    
+    randVec = rand(10,1);
+    
+
+output = [sampleMean, sampleVar, sampleStd];
+endfunction
