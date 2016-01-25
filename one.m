95afdceb691a7e552f3516f279a7eb0996878c69
Octave Online User <webmaster@octave-online.net>
2016-01-25 01:14:49 +0000

    Scripted user file commit

--- one.m
+++ one.m
@@ -1,3 +1,11 @@
-x = (-pi):pi/40:pi
-y = x.*sin(3*x.^2).exp(-x.^2/4);
-plot(x, y)
+m=menu(’Pick a plot’,’Sine plot’,’Cosine plot’);
+if m==1,
+x=(-pi):pi/40:pi;
+y=sin(x);
+title(’Sine’)
+else
+x=(-pi):pi/40:pi;
+y=cos(x);
+title(’Cosine’)
+end
+plot(x,y)
