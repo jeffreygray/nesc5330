9f6996fe552aa703376a59017846a9f4bed2d46e
Octave Online User <webmaster@octave-online.net>
2016-01-25 07:01:29 +0000

    Scripted user file commit

--- calculateAndPlot.m
+++ calculateAndPlot.m
@@ -11,15 +11,12 @@
 
 function output = calculateAndPlot (centerX, centerY, arcLength)
     radius = sqrt((centerX-0).^2 + (centerY-0).^2)
-    arcTheta = arcLength / radius * 180 / pi
-    centerTheta = atan((centerY*180)/(centerX*pi))
+%    arcTheta = arcLength / radius * 180 / pi
+%    centerTheta = atan(centerY/centerX) * 180/pi
+    arcTheta = arcLength / radius 
+    centerTheta = atan(centerY/centerX)
     x_1 = radius * cos(centerTheta - arcTheta/2)
     y_1 = radius * sin(centerTheta - arcTheta/2)
     x_2 = radius * cos(centerTheta + arcTheta/2)
     y_2 = radius * sin(centerTheta + arcTheta/2)
-    
-
-    
-%    newX = radius*(cos(theta*pi/180))
-%    newY = radius*(sin(theta*pi/180))
 endfunction
