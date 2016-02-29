6ebb390adeb8307a28f519356198872c0033f0c6
jeffreygray <jhg7nm@virginia.edu>
2016-02-29 00:23:11 -0500

    adding exploration files

---
+++ exploration/startup.m
@@ -0,0 +1,7 @@
+% Change default axes fonts.
+set(0,'DefaultAxesFontName', 'Consolas')
+set(0,'DefaultAxesFontSize', 14)
+
+% Change default text fonts.
+set(0,'DefaultTextFontname', 'Consolas')
+set(0,'DefaultTextFontSize', 14)
